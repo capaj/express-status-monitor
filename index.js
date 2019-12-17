e882d48aee0cb6e5ca17e18158ca487bf839634c
DEPIDSVY <depidsvy@nintendo.de>
2019-12-17 13:38:12 +0100

    - introduce semicolons

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 var __awaiter =
   (this && this.__awaiter) ||
   function(thisArg, _arguments, P, generator) {
@@ -6,40 +6,40 @@ var __awaiter =
       return value instanceof P
         ? value
         : new P(function(resolve) {
-            resolve(value)
-          })
+            resolve(value);
+          });
     }
     return new (P || (P = Promise))(function(resolve, reject) {
       function fulfilled(value) {
         try {
-          step(generator.next(value))
+          step(generator.next(value));
         } catch (e) {
-          reject(e)
+          reject(e);
         }
       }
       function rejected(value) {
         try {
-          step(generator['throw'](value))
+          step(generator['throw'](value));
         } catch (e) {
-          reject(e)
+          reject(e);
         }
       }
       function step(result) {
         result.done
           ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected)
+          : adopt(result.value).then(fulfilled, rejected);
       }
-      step((generator = generator.apply(thisArg, _arguments || [])).next())
-    })
-  }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
 var __generator =
   (this && this.__generator) ||
   function(thisArg, body) {
     var _ = {
         label: 0,
         sent: function() {
-          if (t[0] & 1) throw t[1]
-          return t[1]
+          if (t[0] & 1) throw t[1];
+          return t[1];
         },
         trys: [],
         ops: []
@@ -47,22 +47,22 @@ var __generator =
       f,
       y,
       t,
-      g
+      g;
     return (
       (g = { next: verb(0), throw: verb(1), return: verb(2) }),
       typeof Symbol === 'function' &&
         (g[Symbol.iterator] = function() {
-          return this
+          return this;
         }),
       g
-    )
+    );
     function verb(n) {
       return function(v) {
-        return step([n, v])
-      }
+        return step([n, v]);
+      };
     }
     function step(op) {
-      if (f) throw new TypeError('Generator is already executing.')
+      if (f) throw new TypeError('Generator is already executing.');
       while (_)
         try {
           if (
@@ -76,72 +76,72 @@ var __generator =
                   : y.next) &&
               !(t = t.call(y, op[1])).done)
           )
-            return t
-          if (((y = 0), t)) op = [op[0] & 2, t.value]
+            return t;
+          if (((y = 0), t)) op = [op[0] & 2, t.value];
           switch (op[0]) {
             case 0:
             case 1:
-              t = op
-              break
+              t = op;
+              break;
             case 4:
-              _.label++
-              return { value: op[1], done: false }
+              _.label++;
+              return { value: op[1], done: false };
             case 5:
-              _.label++
-              y = op[1]
-              op = [0]
-              continue
+              _.label++;
+              y = op[1];
+              op = [0];
+              continue;
             case 7:
-              op = _.ops.pop()
-              _.trys.pop()
-              continue
+              op = _.ops.pop();
+              _.trys.pop();
+              continue;
             default:
               if (
                 !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
                 (op[0] === 6 || op[0] === 2)
               ) {
-                _ = 0
-                continue
+                _ = 0;
+                continue;
               }
               if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
-                _.label = op[1]
-                break
+                _.label = op[1];
+                break;
               }
               if (op[0] === 6 && _.label < t[1]) {
-                _.label = t[1]
-                t = op
-                break
+                _.label = t[1];
+                t = op;
+                break;
               }
               if (t && _.label < t[2]) {
-                _.label = t[2]
-                _.ops.push(op)
-                break
+                _.label = t[2];
+                _.ops.push(op);
+                break;
               }
-              if (t[2]) _.ops.pop()
-              _.trys.pop()
-              continue
+              if (t[2]) _.ops.pop();
+              _.trys.pop();
+              continue;
           }
-          op = body.call(thisArg, _)
+          op = body.call(thisArg, _);
         } catch (e) {
-          op = [6, e]
-          y = 0
+          op = [6, e];
+          y = 0;
         } finally {
-          f = t = 0
+          f = t = 0;
         }
-      if (op[0] & 5) throw op[1]
-      return { value: op[0] ? op[1] : void 0, done: true }
+      if (op[0] & 5) throw op[1];
+      return { value: op[0] ? op[1] : void 0, done: true };
     }
-  }
-Object.defineProperty(exports, '__esModule', { value: true })
-var fs = require('mz/fs')
-var path = require('path')
-var os = require('os')
-var pidusage = require('pidusage')
-var handlebars = require('handlebars')
-var io
-var appName
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+var fs = require('mz/fs');
+var path = require('path');
+var os = require('os');
+var pidusage = require('pidusage');
+var handlebars = require('handlebars');
+var io;
+var appName;
 try {
-  appName = require('../../package.json').name
+  appName = require('../../package.json').name;
 } catch (err) {}
 var defaultConfig = {
   path: '/status',
@@ -166,10 +166,10 @@ var defaultConfig = {
       responses: []
     }
   ]
-}
+};
 var last = function(arr) {
-  return arr[arr.length - 1]
-}
+  return arr[arr.length - 1];
+};
 var gatherOsMetrics = function(io, span) {
   var defaultResponse = {
     '2': 0,
@@ -179,80 +179,80 @@ var gatherOsMetrics = function(io, span) {
     count: 0,
     mean: 0,
     timestamp: Date.now()
-  }
+  };
   var sendMetrics = function(span) {
     io.emit('stats', {
       os: span.os[span.os.length - 2],
       responses: span.responses[span.responses.length - 2],
       interval: span.interval,
       retention: span.retention
-    })
-  }
+    });
+  };
   pidusage(process.pid, function(err, stat) {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
     // console.log(stat);
-    stat.memory = stat.memory / 1024 / 1024 // Convert from B to MB
-    stat.load = os.loadavg()
-    stat.timestamp = Date.now()
-    span.os.push(stat)
+    stat.memory = stat.memory / 1024 / 1024; // Convert from B to MB
+    stat.load = os.loadavg();
+    stat.timestamp = Date.now();
+    span.os.push(stat);
     if (
       !span.responses[0] ||
       last(span.responses).timestamp + span.interval * 1000 < Date.now()
     )
-      span.responses.push(defaultResponse)
-    if (span.os.length >= span.retention) span.os.shift()
+      span.responses.push(defaultResponse);
+    if (span.os.length >= span.retention) span.os.shift();
     if (span.responses[0] && span.responses.length > span.retention)
-      span.responses.shift()
-    sendMetrics(span)
-  })
-}
-var encoding = { encoding: 'utf8' }
+      span.responses.shift();
+    sendMetrics(span);
+  });
+};
+var encoding = { encoding: 'utf8' };
 var middlewareWrapper = function(app, config) {
   if (!app.listen) {
-    throw new Error('First parameter must be an http server')
+    throw new Error('First parameter must be an http server');
   }
-  io = require('socket.io')(app)
-  Object.assign(defaultConfig, config)
-  config = defaultConfig
-  var htmlFilePath = path.join(__dirname, 'index.html')
-  var indexHtml = fs.readFileSync(htmlFilePath, encoding)
-  var template = handlebars.compile(indexHtml)
+  io = require('socket.io')(app);
+  Object.assign(defaultConfig, config);
+  config = defaultConfig;
+  var htmlFilePath = path.join(__dirname, 'index.html');
+  var indexHtml = fs.readFileSync(htmlFilePath, encoding);
+  var template = handlebars.compile(indexHtml);
   io.on('connection', function(socket) {
-    socket.emit('start', config.spans)
+    socket.emit('start', config.spans);
     socket.on('change', function() {
-      socket.emit('start', config.spans)
-    })
-  })
+      socket.emit('start', config.spans);
+    });
+  });
   config.spans.forEach(function(span) {
-    span.os = []
-    span.responses = []
+    span.os = [];
+    span.responses = [];
     var interval = setInterval(function() {
-      return gatherOsMetrics(io, span)
-    }, span.interval * 1000)
-    interval.unref()
-  })
+      return gatherOsMetrics(io, span);
+    }, span.interval * 1000);
+    interval.unref();
+  });
   // console.log(config)
   return function(ctx, next) {
     return __awaiter(void 0, void 0, void 0, function() {
       function record(timeout) {
-        var diff = process.hrtime(startTime)
-        var responseTime = diff[0] * 1e3 + diff[1] * 1e-6
+        var diff = process.hrtime(startTime);
+        var responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
         // if timeout, set response code to 5xx.
-        var category = timeout ? 5 : Math.floor(ctx.statusCode / 100)
+        var category = timeout ? 5 : Math.floor(ctx.statusCode / 100);
         config.spans.forEach(function(span) {
-          var lastResponse = last(span.responses)
+          var lastResponse = last(span.responses);
           if (
             lastResponse &&
             lastResponse.timestamp / 1000 + span.interval > Date.now() / 1000
           ) {
-            lastResponse[category]++
-            lastResponse.count++
+            lastResponse[category]++;
+            lastResponse.count++;
             lastResponse.mean =
               lastResponse.mean +
-              (responseTime - lastResponse.mean) / lastResponse.count
+              (responseTime - lastResponse.mean) / lastResponse.count;
           } else {
             span.responses.push({
               '2': category === 2 ? 1 : 0,
@@ -262,46 +262,46 @@ var middlewareWrapper = function(app, config) {
               count: 1,
               mean: responseTime,
               timestamp: Date.now()
-            })
+            });
           }
-        })
+        });
       }
-      var startTime, pathToJs, _a, timer
+      var startTime, pathToJs, _a, timer;
       return __generator(this, function(_b) {
         switch (_b.label) {
           case 0:
-            startTime = process.hrtime()
-            if (!(ctx.path === config.path)) return [3 /*break*/, 1]
-            ctx.body = template(config)
-            return [3 /*break*/, 5]
+            startTime = process.hrtime();
+            if (!(ctx.path === config.path)) return [3 /*break*/, 1];
+            ctx.body = template(config);
+            return [3 /*break*/, 5];
           case 1:
             if (!(ctx.url === config.path + '/koa-monitor-frontend.js'))
-              return [3 /*break*/, 3]
-            pathToJs = path.join(__dirname, 'koa-monitor-frontend.js')
-            ctx.type = 'js'
-            _a = ctx
-            return [4 /*yield*/, fs.readFile(pathToJs, encoding)]
+              return [3 /*break*/, 3];
+            pathToJs = path.join(__dirname, 'koa-monitor-frontend.js');
+            ctx.type = 'js';
+            _a = ctx;
+            return [4 /*yield*/, fs.readFile(pathToJs, encoding)];
           case 2:
-            _a.body = _b.sent()
-            return [3 /*break*/, 5]
+            _a.body = _b.sent();
+            return [3 /*break*/, 5];
           case 3:
-            timer = void 0
+            timer = void 0;
             if (config.requestTimeout) {
               timer = setTimeout(function() {
-                record.call(ctx, true)
-              }, config.requestTimeout)
+                record.call(ctx, true);
+              }, config.requestTimeout);
             }
-            return [4 /*yield*/, next()]
+            return [4 /*yield*/, next()];
           case 4:
-            _b.sent()
-            timer && clearTimeout(timer)
-            record.call(ctx)
-            _b.label = 5
+            _b.sent();
+            timer && clearTimeout(timer);
+            record.call(ctx);
+            _b.label = 5;
           case 5:
-            return [2 /*return*/]
+            return [2 /*return*/];
         }
-      })
-    })
-  }
-}
-module.exports = middlewareWrapper
+      });
+    });
+  };
+};
+module.exports = middlewareWrapper;
--- index.ts
+++ index.ts
@@ -1,54 +1,54 @@
-'use strict'
+'use strict';
 
-import * as SocketIO from 'socket.io'
-import { Context } from 'koa'
-import Application = require('koa')
+import * as SocketIO from 'socket.io';
+import { Context } from 'koa';
+import Application = require('koa');
 
-const fs = require('mz/fs')
-const path = require('path')
-const os = require('os')
-const pidusage = require('pidusage')
-const handlebars = require('handlebars')
+const fs = require('mz/fs');
+const path = require('path');
+const os = require('os');
+const pidusage = require('pidusage');
+const handlebars = require('handlebars');
 
-let io: SocketIO.Server
-let appName
+let io: SocketIO.Server;
+let appName;
 try {
-  appName = require('../../package.json').name
+  appName = require('../../package.json').name;
 } catch (err) {}
 
 interface Stat {
-  cpu: number
-  memory: number
-  ppid: number
-  pid: number
-  ctime: number
-  elapsed: number
-  timestamp: number
-  load: number[]
+  cpu: number;
+  memory: number;
+  ppid: number;
+  pid: number;
+  ctime: number;
+  elapsed: number;
+  timestamp: number;
+  load: number[];
 }
 
 interface SpanResponse {
-  '2': number
-  '3': number
-  '4': number
-  '5': number
-  count: number
-  mean: number
-  timestamp: number // Date.getTime()
+  '2': number;
+  '3': number;
+  '4': number;
+  '5': number;
+  count: number;
+  mean: number;
+  timestamp: number; // Date.getTime()
 }
 
 interface SpanElement {
-  interval: number
-  retention: number
-  os: any[]
-  responses: SpanResponse[]
+  interval: number;
+  retention: number;
+  os: any[];
+  responses: SpanResponse[];
 }
 
 interface Config {
-  path: string
-  title: string
-  spans: SpanElement[]
-  requestTimeout?: number
+  path: string;
+  title: string;
+  spans: SpanElement[];
+  requestTimeout?: number;
 }
 
 const defaultConfig: Config = {
@@ -74,11 +74,11 @@ const defaultConfig: Config = {
       responses: []
     }
   ]
-}
+};
 
 const last = function(arr: Array<any>) {
-  return arr[arr.length - 1]
-}
+  return arr[arr.length - 1];
+};
 
 const gatherOsMetrics = (io: SocketIO.Server, span: SpanElement) => {
   const defaultResponse: SpanResponse = {
@@ -89,7 +89,7 @@ const gatherOsMetrics = (io: SocketIO.Server, span: SpanElement) => {
     count: 0,
     mean: 0,
     timestamp: Date.now()
-  }
+  };
 
   const sendMetrics = (span: SpanElement) => {
     io.emit('stats', {
@@ -97,105 +97,105 @@ const gatherOsMetrics = (io: SocketIO.Server, span: SpanElement) => {
       responses: span.responses[span.responses.length - 2],
       interval: span.interval,
       retention: span.retention
-    })
-  }
+    });
+  };
 
   pidusage(process.pid, (err: Error | null, stat: Stat) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
     // console.log(stat);
-    stat.memory = stat.memory / 1024 / 1024 // Convert from B to MB
-    stat.load = os.loadavg()
-    stat.timestamp = Date.now()
+    stat.memory = stat.memory / 1024 / 1024; // Convert from B to MB
+    stat.load = os.loadavg();
+    stat.timestamp = Date.now();
 
-    span.os.push(stat)
+    span.os.push(stat);
     if (
       !span.responses[0] ||
       last(span.responses).timestamp + span.interval * 1000 < Date.now()
     )
-      span.responses.push(defaultResponse)
+      span.responses.push(defaultResponse);
 
-    if (span.os.length >= span.retention) span.os.shift()
+    if (span.os.length >= span.retention) span.os.shift();
     if (span.responses[0] && span.responses.length > span.retention)
-      span.responses.shift()
+      span.responses.shift();
 
-    sendMetrics(span)
-  })
-}
+    sendMetrics(span);
+  });
+};
 
-const encoding = { encoding: 'utf8' }
+const encoding = { encoding: 'utf8' };
 
 const middlewareWrapper = (app: Application, config: Config) => {
   if (!app.listen) {
-    throw new Error('First parameter must be an http server')
+    throw new Error('First parameter must be an http server');
   }
-  io = require('socket.io')(app)
-  Object.assign(defaultConfig, config)
-  config = defaultConfig
-  const htmlFilePath = path.join(__dirname, 'index.html')
-  const indexHtml = fs.readFileSync(htmlFilePath, encoding)
-  const template = handlebars.compile(indexHtml)
+  io = require('socket.io')(app);
+  Object.assign(defaultConfig, config);
+  config = defaultConfig;
+  const htmlFilePath = path.join(__dirname, 'index.html');
+  const indexHtml = fs.readFileSync(htmlFilePath, encoding);
+  const template = handlebars.compile(indexHtml);
 
   io.on('connection', (socket) => {
-    socket.emit('start', config.spans)
+    socket.emit('start', config.spans);
     socket.on('change', function() {
-      socket.emit('start', config.spans)
-    })
-  })
+      socket.emit('start', config.spans);
+    });
+  });
 
   config.spans.forEach((span: SpanElement) => {
-    span.os = []
-    span.responses = []
+    span.os = [];
+    span.responses = [];
     const interval = setInterval(
       () => gatherOsMetrics(io, span),
       span.interval * 1000
-    )
-    interval.unref()
-  })
+    );
+    interval.unref();
+  });
   // console.log(config)
 
   return async (ctx: Context, next: Function) => {
-    const startTime = process.hrtime()
+    const startTime = process.hrtime();
 
     if (ctx.path === config.path) {
-      ctx.body = template(config)
+      ctx.body = template(config);
     } else if (ctx.url === `${config.path}/koa-monitor-frontend.js`) {
-      const pathToJs = path.join(__dirname, 'koa-monitor-frontend.js')
-      ctx.type = 'js'
-      ctx.body = await fs.readFile(pathToJs, encoding)
+      const pathToJs = path.join(__dirname, 'koa-monitor-frontend.js');
+      ctx.type = 'js';
+      ctx.body = await fs.readFile(pathToJs, encoding);
     } else {
-      let timer
+      let timer;
       if (config.requestTimeout) {
         timer = setTimeout(() => {
-          record.call(ctx, true)
-        }, config.requestTimeout)
+          record.call(ctx, true);
+        }, config.requestTimeout);
       }
 
-      await next()
+      await next();
 
-      timer && clearTimeout(timer)
-      record.call(ctx)
+      timer && clearTimeout(timer);
+      record.call(ctx);
     }
 
     function record(timeout?: boolean) {
-      const diff = process.hrtime(startTime)
-      const responseTime = diff[0] * 1e3 + diff[1] * 1e-6
+      const diff = process.hrtime(startTime);
+      const responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
       // if timeout, set response code to 5xx.
-      const category = timeout ? 5 : Math.floor(ctx.statusCode / 100)
+      const category = timeout ? 5 : Math.floor(ctx.statusCode / 100);
 
       config.spans.forEach((span: SpanElement) => {
-        const lastResponse = last(span.responses)
+        const lastResponse = last(span.responses);
         if (
           lastResponse &&
           lastResponse.timestamp / 1000 + span.interval > Date.now() / 1000
         ) {
-          lastResponse[category]++
-          lastResponse.count++
+          lastResponse[category]++;
+          lastResponse.count++;
           lastResponse.mean =
             lastResponse.mean +
-            (responseTime - lastResponse.mean) / lastResponse.count
+            (responseTime - lastResponse.mean) / lastResponse.count;
         } else {
           span.responses.push({
             '2': category === 2 ? 1 : 0,
@@ -205,11 +205,11 @@ const middlewareWrapper = (app: Application, config: Config) => {
             count: 1,
             mean: responseTime,
             timestamp: Date.now()
-          })
+          });
         }
-      })
+      });
     }
-  }
-}
+  };
+};
 
-module.exports = middlewareWrapper
+module.exports = middlewareWrapper;
